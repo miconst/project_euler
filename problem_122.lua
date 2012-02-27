cf0d805413e41cff9022be34d1d2cb1238a152a3
Constantin Mikhailov <mi-const@yandex.ru>
2012-02-28 00:01:48 +0400

    problem_122 - optimized

--- problem_122.lua
+++ problem_122.lua
@@ -1,7 +1,7 @@
 -- problem #122
 job_time = os.time()
 
-done = { [1] = { 0, "1" } }
+done = { { 0, "1" }, { 1, "1->2" } }
 
 function get_number_of_multiplications(k)
   if k % 2 == 0 then
@@ -15,44 +15,58 @@ function get_number_of_multiplications(k)
   local path = done[k - 1][2] .. "->" .. tostring(k)
 
   ---print(level_max)
-  local numbers = { [1] = 1 }
-  local tested = {}
+  local numbers = { 1, 2 }
+  --local tested = {}
 
-  local function test(level)
-    local key = table.concat(numbers, "*")
-    if tested[key] then
-      --print(key)
+  local function test(level, tested)
+--    local key = table.concat(numbers, "*", 3, level)
+--    if tested[key] then
+--      --print(key)
+--      return
+--    else
+--      tested[key] = true
+--    end
+    if tested[numbers[level]] then
       return
     else
-      tested[key] = true
+      tested[numbers[level]] = {}
     end
-    for i = #numbers, 1, -1 do
-      for j = #numbers, 1, -1 do
+
+    local num_hash = {}
+    for i = level, 1, -1 do
+      for j = level, i, -1 do
         local n = numbers[i] + numbers[j]
-        if level >= level_max then
-          return
-        elseif n == k then
+        --if level >= level_max then
+        --  return
+        --else
+        if n == k then
           -- gotcha
           level_max = level
-          table.insert(numbers, n)
-          path = table.concat(numbers, "->")
-          table.remove(numbers)
+          numbers[level + 1] = n
+          path = table.concat(numbers, "->", 1, level + 1)
           return
-        elseif level + 1 < level_max and n < k and n > numbers[#numbers] then
-          table.insert(numbers, n)
-          test(level + 1)
-          table.remove(numbers)
+        elseif level + 1 < level_max and n < k and n > numbers[level] then
+          num_hash[n] = true
         end
       end
     end
+    tested = tested[numbers[level]]
+    level = level + 1
+    if level < level_max then
+      for n in pairs(num_hash) do
+        numbers[level] = n
+        test(level, tested)
+      end
+    end
   end
-  test(1)
+  test(#numbers, {})
   done[k] = { level_max, path }
+
   return level_max, path
 end
 
-sum = 0
-for i = 2, 200 do
+sum = 1
+for i = 3, 200 do
   local s, p = get_number_of_multiplications(i)
   print(i, s, p)
   sum = sum + s
