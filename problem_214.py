5192abff83546d598ebf7ab7d32d72c5c0fb0504
micon <4.micon@gmail.com>
2013-10-26 21:10:29 +0400

    problem #214 optimized

--- problem_214.py
+++ problem_214.py
@@ -17,53 +17,30 @@ def get_primes( n ):
         primes.discard( j )
 
   primes.add( 2 )
+
   return primes
 
-primes = get_primes( N )
-#~ print(len(primes))
+primes = get_primes( N - 1 )
 
 # The General formula for Euler's Totient Function is:
 # euler_phi(m) = m * (1 - 1 / p1) * (1 - 1 / p2) * (1 - 1 / p3) * ( ... ) * (1 - 1 / pn)
 # Where p1, p2, p3, . . . , pn are prime factors of m.
-
-def euler_phi(m):
-  phi = m
-  i = iter(primes)
-  while m != 1:
-    if m in primes:
-      phi -= phi / m
-      m = 1
-    else:
-      while True:
-        p = i.next()
-        if m % p == 0:
-          while m % p == 0:
-            m /= p
-          phi -= phi / p
-          break
-  return phi
+phi = range( N )
+for p in primes:
+  for i in xrange( p, N, p ):
+    phi[ i ] -= phi[ i ] / p
 
 done = { 1:1 }
 
 def euler_phi_chain_length(m):
   if m not in done:
-    c = 0
-    n = m
-    while n not in done:
-      n = euler_phi(n)
-      c += 1
-    done[m] = c + done[n]
+    done[m] = 1 + euler_phi_chain_length(phi[m])
   return done[m]
 
-c = 0
-for p in sorted(primes):
+for p in primes:
   l = euler_phi_chain_length(p)
   if l == L:
-    c += 1
-    #~ print(p)
     S += p
-
-#~ print "!", c
 
 print("problem #214. " +
       "The sum of all primes less than {0} which generate a chain of length {1}: {2}".format(N, L, S), S == 1677366278943)
