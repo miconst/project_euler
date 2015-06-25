a94135447e63ea8f84b812c38a1d588ab4fea1cb
mico <mi-const@yandex.ru>
2015-06-25 11:48:00 +0300

    cleaning

--- problem_347.py
+++ problem_347.py
@@ -21,39 +21,20 @@ def get_primes( n ):
 
 primes = sorted(get_primes(N/2))
 
-# M = dict()
-
 for i in xrange(len(primes) - 1):
     a = primes[i]
-    # assert(not a in M)
-    # M[a] = dict()
     for j in xrange(i + 1, len(primes)):
         b = primes[j]
-        if a*b > N:
+        m = a*b
+        if m > N:
             break
-        
-        assert(a < b and a*b <= N)
 
-        # assert(not b in M[a])
-        # M[a][b] = a*b
-        M = a*b
         for ka in range(1, int(math.log(N, a)) + 1):
             for kb in range(1, int(math.log(N, b)) + 1):
                 n = a**ka * b**kb
-                if M < n <= N:
-                    M = n
-        S += M
-
-# for a in M:
-    # print(a,":", M[a])
-    # for b in M[a]:
-        # S += M[a][b]
-
-# print(M[2][3])
-# print(M[3][5])
-# print(M[2][73])
-
-# print(M)
+                if m < n <= N:
+                    m = n
+        S += m
 
 print("problem #347. " +
       "The sum of all distinct M(p,q,N) for n <= {0}: {1} ({2})".format(N, S, S == 11109800204052))
